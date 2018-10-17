90e1e4f817d0ae8489a96b1ac4730207dd75877a
Leland Later <lelandlater@gmail.com>
2018-10-17 00:01:06 -0700

    Remove sptfy.go. This is a package

--- src/sptfy/sptfy.go
+++
@@ -1,9 +0,0 @@
-package sptfy
-
-type Sptfy struct{}
-
-// Returns whether a Spotify user exists with this id, then the user, and an error.
-// Use to check if a user exists with a given Spotify user id.
-func (s *Sptfy) GetUserById(spotifyId string) (bool, *SptfyUser, error) {
-   
-}
